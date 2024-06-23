120cc5afe5c5459f1003e70701e2a7beb085c0fd
Bogamie <bogamie02@gmail.com>
2024-06-23 23:49:39 +0900

    Fix: Resolve header not staying fixed at the top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
 const indicator = document.querySelector('.nav-indicator');
 const navItems = document.querySelectorAll('.nav-item');
+const header = document.querySelector(".masthead");
 const windowPathName = window.location.pathname;
 
-navItems.forEach (navItem => {
+navItems.forEach(navItem => {
     const navLinkPathName = new URL(navItem.href).pathname;
 
     if ((windowPathName === navLinkPathName) || (navLinkPathName === '/')) {
@@ -12,3 +13,7 @@ navItems.forEach (navItem => {
         indicator.style.left = `${navItem.offsetLeft + 30}px`;
     }
 })
+
+window.addEventListener("scroll", function () {
+    header.classList.toggle("stickyHead", window.scrollY > 60);
+});
