11012241578efe1a6b017a2e1010c5bf99771681
Bogamie <bogamie02@gmail.com>
2024-08-04 15:25:30 +0900

    Fix: Resolve flicker when changing themes

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -2,20 +2,20 @@ let darkmode = localStorage.getItem('darkmode')
 const themeSwitch = document.getElementById('theme-switch')
 
 const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
+    document.documentElement.classList.add('darkmode')
     localStorage.setItem('darkmode', 'active')
 }
 
 const disableDarkmode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
+    document.documentElement.classList.remove('darkmode')
+    localStorage.setItem('darkmode', 'inactive')
 }
 
-if (darkmode === "active") {
+if (darkmode === 'active') {
     enableDarkmode()
 }
 
-themeSwitch.addEventListener("click", () => {
+themeSwitch.addEventListener('click', () => {
     darkmode = localStorage.getItem('darkmode')
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+    darkmode !== 'active' ? enableDarkmode() : disableDarkmode()
 })
